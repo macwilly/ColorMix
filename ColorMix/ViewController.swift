f84be75206316f73c7ae8d2e45e63061b2b7b1e1
Mackenzie Willard <mtwillard29@gmail.com>
2022-03-10 19:51:21 -0500

    Initial Commit

---
+++ ColorMix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorMix
+//
+//  Created by Mackenzie Willard on 3/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
