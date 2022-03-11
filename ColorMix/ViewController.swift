670151430cc4dafee21560b06479dc7c7bca4cb5
Mackenzie Willard <mtwillard29@gmail.com>
2022-03-10 20:04:20 -0500

    moving the placement of the outlet

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -8,13 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBOutlet weak var colorView: UIView!
+    
     
 }
 
