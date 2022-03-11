9bfbea76fefcd7e46797eef8687c4b79a33c580d
Mackenzie Willard <mtwillard29@gmail.com>
2022-03-10 21:18:13 -0500

    adding on the new function to make the color and refactoring the code to use the function

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -17,15 +17,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        colorView.backgroundColor = .black
+        updateColor()
     }
 
     @IBAction func switchChange(_ sender: UISwitch) {
-        if sender.isOn{
-            colorView.backgroundColor = .red
-        } else {
-            colorView.backgroundColor = .black
+        updateColor()
+    }
+    
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSwitch.isOn {
+            red = 1
         }
+        
+        if greenSwitch.isOn {
+            green = 1
+        }
+        
+        if blueSwitch.isOn {
+            blue = 1
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
     }
     
     
